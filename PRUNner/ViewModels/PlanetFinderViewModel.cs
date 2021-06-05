a4bac7e298b60352feee30d023069057ef6a0952
Jacudibu <jacudibu@gmail.com>
2021-06-05 12:08:51 +0200

    Updating Avalonia... :D

---
+++ PRUNner/ViewModels/PlanetFinderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRUNner.ViewModels
+{
+    public class PlanetFinderViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}

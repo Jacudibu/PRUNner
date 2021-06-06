fcd21f3f796e0d6ecf2f174344a3da2dd8880270
Jacudibu <jacudibu@gmail.com>
2021-06-06 22:46:06 +0200

    Began work on the Building Planner

---
+++ PRUNner/ViewModels/BasePlannerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PRUNner.Backend.Data;
+using PRUNner.Models;
+using PRUNner.Models.BasePlanner;
+
+namespace PRUNner.ViewModels
+{
+    public class BasePlannerViewModel : ViewModelBase
+    {
+        public PlanetaryBase ActiveBase { get; private set; }
+
+        public BuildingTextBox AddBuildingTextBox { get; } = new();
+        
+        public void StartNewBase(PlanetData planetData)
+        {
+            ActiveBase = new PlanetaryBase(planetData);
+        }
+
+        public void AddBuilding()
+        {
+            if (AddBuildingTextBox.Building == null)
+            {
+                return;
+            }
+
+            ActiveBase.AddBuilding(AddBuildingTextBox.Building);
+        }
+    }
+}

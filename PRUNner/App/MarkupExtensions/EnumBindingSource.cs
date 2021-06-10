fc0f65e204eda099443f3b4b938bfa0d21e12798
Jacudibu <jacudibu@gmail.com>
2021-06-10 16:53:19 +0200

    Added EnumBindingSource

---
+++ PRUNner/App/MarkupExtensions/EnumBindingSource.cs
@@ -0,0 +1,53 @@
+using System;
+using Avalonia;
+
+namespace PRUNner.App.MarkupExtensions
+{
+    // Taken from https://log.koepferl.de/2020/09/20/mit-avaloniaui-enums-in-xaml-zeigen/
+    public class EnumBindingSource : AvaloniaObject
+    {
+        private Type _enumType;
+        public Type EnumType
+        {
+            get { return this._enumType; }
+            set
+            {
+                if (value != this._enumType)
+                {
+                    if (null != value)
+                    {
+                        Type enumType = Nullable.GetUnderlyingType(value) ?? value;
+
+                        if (!enumType.IsEnum)
+                            throw new ArgumentException("Type must be for an Enum.");
+                    }
+
+                    this._enumType = value;
+                }
+            }
+        }
+
+        public EnumBindingSource() { }
+
+        public EnumBindingSource(Type enumType)
+        {
+            this.EnumType = enumType;
+        }
+
+        public Array ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (null == this._enumType)
+                throw new InvalidOperationException("The EnumType must be specified.");
+
+            Type actualEnumType = Nullable.GetUnderlyingType(this._enumType) ?? this._enumType;
+            Array enumValues = Enum.GetValues(actualEnumType);
+
+            if (actualEnumType == this._enumType)
+                return enumValues;
+
+            Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
+            enumValues.CopyTo(tempArray, 1);
+            return tempArray;
+        }
+    }
+}

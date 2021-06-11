51c3f3aff1ab8f59bbb0f53f0f82990b91bc08c9
Jacudibu <jacudibu@gmail.com>
2021-06-11 12:43:36 +0200

    Publish script now also immediately zips results

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-VERSION=0.1.0
+VERSION=0.1.1
 dotnet publish PRUNner -c Release -o Release/PRUNner-$VERSION-linux-64 -f net5.0 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:Version=$VERSION --self-contained true -r linux-x64 
 dotnet publish PRUNner -c Release -o Release/PRUNner-$VERSION-win-x64 -f net5.0 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:Version=$VERSION --self-contained true -r win-x64
 dotnet publish PRUNner -c Release -o Release/PRUNner-$VERSION-osx-x64 -f net5.0 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:Version=$VERSION --self-contained true -r osx-x64
@@ -6,5 +6,10 @@ dotnet publish PRUNner -c Release -o Release/PRUNner-$VERSION-osx-x64 -f net5.0 
 cp -r FIOCache Release/PRUNner-$VERSION-linux-64/FIOCache
 cp -r FIOCache Release/PRUNner-$VERSION-win-x64/FIOCache
 cp -r FIOCache Release/PRUNner-$VERSION-osx-x64/FIOCache
+
+cd Release || exit
+zip PRUNner-$VERSION-linux-64.zip -r PRUNner-$VERSION-linux-64
+zip PRUNner-$VERSION-win-x64.zip -r PRUNner-$VERSION-win-x64
+zip PRUNner-$VERSION-osx-x64.zip -r PRUNner-$VERSION-osx-x64
 
 echo "DONE! :)"

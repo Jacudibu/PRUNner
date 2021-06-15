cad557ebe25e890db46cce6d212303915f9e79c9
Jacudibu <jacudibu@gmail.com>
2021-06-15 22:47:36 +0200

    We are now hiding the Base Planner if no active base is selected

---
+++ PRUNner/App/Converters/IsNotNullConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace PRUNner.App.Converters
+{
+    public class IsNotNullConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null;
+        }
+    }
+}

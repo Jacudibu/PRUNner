d5ac1f9c8fcd455c318aeba3052dcb8e6fcda81e
Jacudibu <jacudibu@gmail.com>
2021-07-14 21:50:55 +0200

    Fixed shopping cart remaining amounts not updating on load

--- PRUNner/Backend/BasePlanner/ShoppingCart/ShoppingCartMaterial.cs
+++ PRUNner/Backend/BasePlanner/ShoppingCart/ShoppingCartMaterial.cs
@@ -8,18 +8,36 @@ namespace PRUNner.Backend.BasePlanner.ShoppingCart
     public class ShoppingCartMaterial : ReactiveObject
     {
         public MaterialData Material { get; }
-        [Reactive] public int TotalAmount { get; internal set; }
         [Reactive] public int RemainingAmount { get; private set; }
 
+        private int _totalAmount;
         private int _inventory;
+
+        public int TotalAmount
+        {
+            get => _totalAmount;
+            internal set
+            {
+                _totalAmount = value;
+                RecalculateRemainingAmount();
+                this.RaisePropertyChanged(nameof(TotalAmount));
+            }
+        }
+
+        
         public int Inventory
         {
             get => _inventory;
             set
             {
                 _inventory = value;
-                RemainingAmount = Math.Max(0, TotalAmount - _inventory);
+                RecalculateRemainingAmount();
             }
+        }
+
+        private void RecalculateRemainingAmount()
+        {
+            RemainingAmount = Math.Max(0, TotalAmount - _inventory);
         }
 
         public ShoppingCartMaterial(MaterialData material)

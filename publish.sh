ad0ef92ed2afa309babedaae1b109f8c2e8ad27f
Jacudibu <jacudibu@gmail.com>
2021-10-11 13:10:51 +0200

    Version bump

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-VERSION=0.3.7
+VERSION=0.3.8
 dotnet publish PRUNner -c Release -o Release/PRUNner-$VERSION-linux-64 -f net5.0 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:Version=$VERSION --self-contained true -r linux-x64 
 dotnet publish PRUNner -c Release -o Release/PRUNner-$VERSION-win-x64 -f net5.0 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:Version=$VERSION --self-contained true -r win-x64
 dotnet publish PRUNner -c Release -o Release/PRUNner-$VERSION-osx-x64 -f net5.0 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:Version=$VERSION --self-contained true -r osx-x64

be08a1fe39f6e58c89637ac450a17859a9299134
Jacudibu <jacudibu@gmail.com>
2022-05-04 15:18:07 +0200

    Added production line age in days, condition is now automagically calculated based on the entered days

--- PRUNner/Backend/BasePlanner/AdvancedBuildingConfiguration.cs
+++ PRUNner/Backend/BasePlanner/AdvancedBuildingConfiguration.cs
@@ -1,3 +1,4 @@
+using System;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
@@ -5,8 +6,42 @@ namespace PRUNner.Backend.BasePlanner
 {
     public class AdvancedBuildingConfiguration : ReactiveObject
     {
-        [Reactive] public double ProductionLineCondition { get; set; } = 100;
+        private double _productionLineCondition = 100;
+        private double _productionLineAge = 0;
+
+        public double ProductionLineCondition
+        {
+            get => _productionLineCondition;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _productionLineCondition, value, nameof(ProductionLineCondition));
+                RecalculateAgeBasedOnCondition();
+            }
+        }
+
+        public double ProductionLineAge
+        {
+            get => _productionLineAge;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _productionLineAge, value, nameof(ProductionLineAge));
+                RecalculateConditionBasedOnAge();
+            }
+        }
+
         [Reactive] public bool UseEfficiencyOverride { get; set; }
         [Reactive] public double EfficiencyOverride { get; set; } = 100;
+
+        private const double C = 100.87;
+        private void RecalculateConditionBasedOnAge()
+        {
+            _productionLineCondition = 100 * ((2d / 3d) / (1 + Math.Exp(0.07156 * (_productionLineAge - C))) + (1d / 3d));
+            this.RaisePropertyChanged(nameof(ProductionLineCondition));
+        }
+
+        private void RecalculateAgeBasedOnCondition()
+        {
+            // TODO, that way people could figure out how many days it would take to reach a certain condition
+        }
     }
 }
